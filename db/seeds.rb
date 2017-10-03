ec6159ea95d2fa42c91dba065dfd92684d0c692b
JemimaEllis <jemimaeellis@gmail.com>
2017-10-03 22:05:16 +0100

    working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-c1 = Cats.create!(name: "Toffee", age: 2, gender: "Female", size: 10)
-c2 = Cats.create!(name: "Stripe", age: 8, gender: "Female", size: 5)
-c3 = Cats.create!(name: "Dusty", age: 23, gender: "Male", size: 15)
+c1 = Cat.create!(name: "Toffee", age: 2, gender: "Female", size: 10)
+c2 = Cat.create!(name: "Stripe", age: 8, gender: "Female", size: 5)
+c3 = Cat.create!(name: "Dusty", age: 23, gender: "Male", size: 15)
 
-h1 = Homes.create!(address: "14, blob lane", catflatsize: 20)
-h2 = Homes.create!(address: "15, blob lane", catflatsize: 16)
+h1 = Home.create!(address: "14, blob lane", catflatsize: 20)
+h2 = Home.create!(address: "15, blob lane", catflatsize: 16)
 
 puts "Finished Seed..."
