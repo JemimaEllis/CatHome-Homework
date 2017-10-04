ee0ba6da1b4c3a90c2723bdd9f939c044ec37b9e
JemimaEllis <jemimaeellis@gmail.com>
2017-10-04 16:41:09 +0100

    Here you go Jenny

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,14 @@ c3 = Cat.create!(name: "Dusty", age: 23, gender: "Male", size: 15)
 h1 = Home.create!(address: "14, blob lane", catflatsize: 20)
 h2 = Home.create!(address: "15, blob lane", catflatsize: 16)
 
+
+CatHome.create(cat_id: c1.id, home: h1.id)
+CatHome.create(cat_id: c2.id, home: h2.id)
+CatHome.create(cat_id: c3.id, home: h1.id)
+
+
+Person.create!(name: "Jemima", age 21, home_id: h1.id)
+Person.create!(name: "Timothy", age 24, home_id: h1.id)
+Person.create!(name: "Lindsey", age 22, home_id: h2.id)
+
 puts "Finished Seed..."
